bf1ebde34e7f9604a1e3a1c2c66d4eca67cf46d0
Ashlee <ashleeramirezrosario@gmail.com>
2025-07-31 17:33:49 -0400

    Renombre algunas carpetas e hice una config delmotor en app.jss

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ import { engine } from 'express-handlebars';
 import path from 'path';
 
 const app = express();
+
+app.engine("hbs", engine({
+    layoutsDir: "views/layouts",
+    defaultLayout: "MainLayout",
+    extname: ".hbs",
+    helpers: {},
+}));
+
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+//Routes
+
+
+//404
+
+
+//DB Configs
