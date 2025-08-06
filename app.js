108ad50a2093d8eb899d71c2852b97fa563b39d2
GGeva07 <evaGO07@hotmail.com>
2025-08-05 23:54:25 -0400

    adding gitignore

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(multer({ storage: UserMulter }).single("UserProfilePhoto"));
 
 //404
 app.use((req, res, next) => {
-  res.status(404).render("404", { "page.title": "Not found" });
+  res.status(404).render("404", { "page-title": "Not found" });
 });
 
 //DB Configs
