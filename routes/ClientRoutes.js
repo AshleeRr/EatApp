a6d270aadabbea57243c558a5f80e80c1343b29b
GGeva07 <evaGO07@hotmail.com>
2025-08-13 14:40:44 -0400

    otro conlicto

--- routes/ClientRoutes.js
+++ routes/ClientRoutes.js
@@ -1,12 +1,2 @@
 import express from "express";
-//import { GetIndex } from '../controllers/ClientController.js';
-import express from "express";
-// import { GetProfile, GetDirections } from "../controllers/ClientController.js";
-
-import isAuth from "../middlewares/isAuthenticated.js";
-
-const router = express.Router();
-
-//router.get('/home',isAuth, GetIndex); //isAuth A Todas las rutas
-
-export default router;
+import { GetProfile, GetDirections } from "../controllers/ClientController.js";
