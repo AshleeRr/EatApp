08cb6cc9f02a957d86cbdb8254906e70b7f38355
Ashlee <ashleeramirezrosario@gmail.com>
2025-08-13 18:48:53 -0400

    .

--- controllers/ClientController.js
+++ controllers/ClientController.js
@@ -2,10 +2,19 @@ import context from "../config/context/AppContext.js";
 
 export async function GetHome(req,res,next){
 
+  const typeFilter = req.query.typeFilter || '';
+  const result = {};
+
+  if(typeFilter.trim()){
+    const businessTypeId = Array.isArray(typeFilter) 
+      ? typeFilter.map(Number) : [Number(typeFilter)];
+      result.businessTypesId = {[Op.in]: businessTypeId}
+  }
+
   const businesses = await context.Comercio.findAll();
   const businessTypes = await context.TipoComercio.findAll();
   
-  const businessTypesPlain = businessTypes.map((bt) => bt.get({plain: true}));
+  //const businessTypesPlain = businessTypes.map((bt) => bt.get({plain: true}));
   const businessesPlain = businesses.map((b)=> b.get({plain: true}));
 
     return res.render(
@@ -14,8 +23,8 @@ export async function GetHome(req,res,next){
         layout: "ClientLayout",
       //  businessesList: businesses,
         //hasbusinesses: businesses.length > 0,
-        businessTypeList: businessTypesPlain,
-        hasBusinessTypes: businessTypesPlain.length > 0 
+        businessTypeList: businessTypes,
+        hasBusinessTypes: businessTypes.length > 0 
       });
 }
 
