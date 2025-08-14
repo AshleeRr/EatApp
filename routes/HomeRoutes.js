518dabd5858967c5f68dcd133ab29bde8c180c0a
Ashlee <ashleeramirezrosario@gmail.com>
2025-08-14 13:15:56 -0400

    avance en el home del cliente, se elimino lo relacionado a home ya que ya no es necesario

--- routes/HomeRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { GetIndex } from "../controllers/HomeController.js";
-
-const router = express.Router();
-
-router.get("/home", GetIndex);
-
-export default router;
